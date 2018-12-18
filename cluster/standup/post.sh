2cf912b565271394857e3ef0f18c272d870ed3e0
Bob Henkel <bhenkel@peoplenetonline.com>
2018-12-17 22:41:32 -0600

    Add place holder and bump post.sh

--- cluster/standup/post.sh
+++ cluster/standup/post.sh
@@ -2,3 +2,7 @@
 
 #Installs "god" tiller into tools namespaces
 helm init --tiller-namespace kuberchaun-tools --service-account tiller
+
+#Sealed secrets cluster bits -> https://github.com/bitnami-labs/sealed-secrets
+release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
+kubectl create -f https://github.com/bitnami-labs/sealed-secrets/releases/download/$release/sealedsecret-crd.yaml
