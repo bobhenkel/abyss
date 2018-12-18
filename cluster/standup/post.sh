cb91e043039cfa1718f4210c8549d3989592bb32
Bob Henkel <bhenkel@peoplenetonline.com>
2018-12-17 23:15:07 -0600

    Updated post.sh

--- cluster/standup/post.sh
+++ cluster/standup/post.sh
@@ -6,3 +6,8 @@ helm init --tiller-namespace kuberchaun-tools --service-account tiller
 #Sealed secrets cluster bits -> https://github.com/bitnami-labs/sealed-secrets
 release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
 kubectl create -f https://github.com/bitnami-labs/sealed-secrets/releases/download/$release/sealedsecret-crd.yaml
+
+USER_EMAIL=Bob.Henkel@gmail.com
+kubectl create clusterrolebinding $USER-cluster-admin-binding --clusterrole=cluster-admin --user=$USER_EMAIL
+
+kubectl create -f https://github.com/bitnami-labs/sealed-secrets/releases/download/$release/controller.yaml
